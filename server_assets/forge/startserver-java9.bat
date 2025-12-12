1285bff210f758e59bcc3bd155a236b1a4bf8338
PLASMAchicken <plasmachickenmc@gmail.com>
2025-12-12 22:04:21 +0100

    Update startserver-java9.bat with nogui reminder and pause

--- server_assets/forge/startserver-java9.bat
+++ server_assets/forge/startserver-java9.bat
@@ -1,2 +1,4 @@
 rem don't forget to accept the EULA or it won't boot
+rem don't remove the nogui as it might cause lag issues
 java -Xms6G -Xmx6G -Dfml.readTimeout=180 @java9args.txt -jar lwjgl3ify-forgePatches.jar nogui
+pause

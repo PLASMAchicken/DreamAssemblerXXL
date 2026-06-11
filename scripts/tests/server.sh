65b23e5fc8f3d480c5d312b13cec45b77641d603
MalTeeez <41554385+MalTeeez@users.noreply.github.com>
2026-06-11 04:26:10 +0200

    Fix uploading screenshots as zip, test out some more way of showing the images in the summary, set server to offline mode, make summary collapseable

--- scripts/tests/server.sh
+++ scripts/tests/server.sh
@@ -33,6 +33,7 @@ start() {
   cd "$SERVER_DIR"
   sed -i 's|eula=false|eula=true|g' eula.txt
   sed -i "s|enable-rcon=false|enable-rcon=true\nrcon.password=${RCON_PASSWORD}\nrcon.port=${RCON_PORT}|" server.properties
+  sed -i 's|online-mode=true|online-mode=false|' server.properties
 
   # Backgrounded and logged to a file so this CI step can finish while the JVM
   # keeps running into the next steps.
